03c639e07a77fe413e4301e4fbebe995d5b1345c
clementacm <clementacmanne26@gmail.com>
2025-12-25 09:49:17 +0100

    update_25-12-2025
    update...

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ num_sae = Object.keys(SAE);
 console.log(num_sae);
 
 // Affichage des dossiers SAE
-
+/*
 let ligne = "";
 
 num_sae.forEach(function (element, i) {
     ligne += "<a href='sae.html?sae="+element+"'><div class='dossier'><img src='img/paper_folder.png' class='img_dossier'><h2>" + num_sae[i] + "</h2></div></a>";
-   /*ligne += "<div><svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 24 24' fill='none' stroke='#F9F5ED' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path></svg>'<a href='sae.html'><h2>" + num_sae[i] + "</h2></a></div>";*/
+   ligne += "<div><svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 24 24' fill='none' stroke='#F9F5ED' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path></svg>'<a href='sae.html'><h2>" + num_sae[i] + "</h2></a></div>";
 })
 
 document.querySelector(".zone_sae").innerHTML = ligne;
+*/
--- js/scriptSAE.js
+++ js/scriptSAE.js
@@ -1,50 +1,50 @@
 // Génération de la page SAE
-let url= new URLSearchParams(location.search);
+
+let url = new URLSearchParams(location.search);
 let page = url.get('sae');
 
 let num_sae = Object.keys(SAE);
 
 let ressources = Object.keys(SAE[page]["ressources"]);
 
+document.querySelector("h1").innerHTML = page;
+document.querySelector("h2").innerHTML = SAE[page]["titre"];
+document.querySelector(".competence").innerHTML = SAE[page]["compétences"];
+document.querySelector(".description>p").innerHTML = SAE[page]["description"];
 
-document.querySelector("h1").innerHTML=page;
-document.querySelector("h2").innerHTML=SAE[page]["titre"];
-document.querySelector(".competence").innerHTML=SAE[page]["compétences"];
-document.querySelector(".description>p").innerHTML=SAE[page]["description"];
+// Tableau des ressources
 
 let tableau_ressources = "";
 
-ressources.forEach(function(ressource, i)
-{
-    tableau_ressources += "<tr><td>"+ressource+"</td><td>"+SAE[page]["ressources"][ressource]+"</td></tr>";
+ressources.forEach(function (ressource, i) {
+  tableau_ressources += "<tr><td>" + ressource + "</td><td>" + SAE[page]["ressources"][ressource] + "</td></tr>";
 })
 
-document.querySelector(".ressources").innerHTML="<table>"+tableau_ressources+"</table>";
+document.querySelector(".tableau").innerHTML = "<table>" + tableau_ressources + "</table>";
+
+// Section Apprentissages Critiques en accordéon
 
 // Bouton ouvrir le document
 
-document.querySelector(".btn").innerHTML="<a href='../pdf/"+page+".pdf' target='_blank'><div class='btn_ouvrirAC'>Ouvrir le document <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='none' stroke='#F5F5DC' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><g fill='none' fill-rule='evenodd'><path d='M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8'/></g></svg></div></a>"
-
-// Section Apprentissages Critiques en accordéon
+document.querySelector(".bouton").innerHTML = "<a href='../pdf/" + page + ".pdf' target='_blank'><div>Ouvrir le document</div><svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='none' stroke='#F5F5DC' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><g fill='none' fill-rule='evenodd'><path d='M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8'/></g></svg></a>";
 
 let apprentissages = Object.keys(SAE[page]["AC"]);
 
 let descriptionsAC = Object.keys(ACRITIQUES[page]);
 
-let tableau_ac = "";
+let accordeon_ac = "";
 
-apprentissages.forEach(function(ac, i)
-{
-    tableau_ac += "<button class='accordeon'>"+apprentissages[i]+" - "+SAE[page]["AC"][ac]+"</button><div class='contenu'><p>Description de l'AC : "+ACRITIQUES[page][ac]+"</p></div>";
+apprentissages.forEach(function (ac, i) {
+  accordeon_ac += "<button class='accordeon'>" + apprentissages[i] + " - " + SAE[page]["AC"][ac] + "</button><div class='contenu'><p>Description de l'AC : " + ACRITIQUES[page][ac] + "</p></div>";
 })
 
-document.querySelector(".ac").innerHTML=tableau_ac;
+document.querySelector(".ac").innerHTML = accordeon_ac;
 
 var ac = document.getElementsByClassName("accordeon");
 var i;
 
 for (i = 0; i < ac.length; i++) {
-  ac[i].addEventListener("click", function() {
+  ac[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -57,4 +57,6 @@ for (i = 0; i < ac.length; i++) {
       contenu.style.display = "block";
     }
   });
-} 
+}
+
+//
