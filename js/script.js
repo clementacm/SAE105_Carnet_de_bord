f4e12d431023b36f84983086477b1c4cd91ee699
clementacm <clementacmanne26@gmail.com>
2026-01-05 21:19:21 +0100

    update_05-01-2026-2130

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
-num_sae = Object.keys(SAE);
-console.log(num_sae);
+// Récupération des données
 
-// Affichage des dossiers SAE
-/*
-let ligne = "";
+let num_sae = Object.keys(SAE);
 
-num_sae.forEach(function (element, i) {
-    ligne += "<a href='sae.html?sae="+element+"'><div class='dossier'><img src='img/paper_folder.png' class='img_dossier'><h2>" + num_sae[i] + "</h2></div></a>";
-   ligne += "<div><svg xmlns='http://www.w3.org/2000/svg' width='100' height='100' viewBox='0 0 24 24' fill='none' stroke='#F9F5ED' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'><path d='M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z'></path></svg>'<a href='sae.html'><h2>" + num_sae[i] + "</h2></a></div>";
-})
+// Création de le grille Bento
 
-document.querySelector(".zone_sae").innerHTML = ligne;
-*/
+let grille = "";
+
+num_sae.forEach(function(id, i) {
+    // Par défaut, toutes les cases sont au format "compacte"
+    let classe_case = "compacte";
+
+    // Indices correspondant aux cases larges
+    if (i == 0 || i == 8 || i == 9 || i == 17){
+        classe_case = "bandeau";
+    }
+    // Indices correspondants aux cases hautes
+    else {
+        if (i == 4 || i == 11){
+            classe_case = "portrait";
+        }
+    }
+    grille += "<a href='sae.html?sae=" + id + "' class='case " + classe_case + "'>" + id + "</a>";
+
+});
+
+document.querySelector(".grille_sae").innerHTML = grille;
--- js/scriptSAE.js
+++ js/scriptSAE.js
@@ -3,16 +3,19 @@
 let url = new URLSearchParams(location.search);
 let page = url.get('sae');
 
-let num_sae = Object.keys(SAE); // Récupère la liste des SAE (SAE1.01, SAE1.02 etc...)
+let num_sae = Object.keys(SAE);
+let ressources = Object.keys(SAE[page]["ressources"]);
 
-let ressources = Object.keys(SAE[page]["ressources"]); // Récupère les numéros des ressources de la SAE choisie (R1.01, R1.02 etc...)
+/*******************************************/
+/*           Affichage du header           */
+/*******************************************/
 
+// Numéro et titre
 document.querySelector("h1").innerHTML = page;
 document.querySelector("h2").innerHTML = SAE[page]["titre"];
 
+// Compétence(s)
 let competences = SAE[page]["compétences"];
-console.log(competences);
-
 let affichage_competences = "";
 
 competences.forEach(function (competence) {
@@ -25,12 +28,14 @@ if (competences.length > 1){
   document.querySelector(".zone_competences>h3").innerHTML = "Compétences : ";
 }
 
-/*document.querySelector(".competence").innerHTML = "<div>"+SAE[page]["compétences"]+"</div>";*/
+/*******************************************/
+/*            Section principale           */
+/*******************************************/
 
+// Description
 document.querySelector(".description>p").innerHTML = SAE[page]["description"];
 
 // Tableau des ressources
-
 let tableau_ressources = "";
 
 ressources.forEach(function (ressource, i) { // Pour chaque ressource => création d'une ligne de tableau avec deux cases
@@ -39,41 +44,44 @@ ressources.forEach(function (ressource, i) { // Pour chaque ressource => créati
 
 document.querySelector(".tableau").innerHTML = "<table>" + tableau_ressources + "</table>";
 
-// Section Apprentissages Critiques en accordéon
+/*******************************************/
+/*        Apprentissages critiques         */
+/*******************************************/
 
-// Bouton ouvrir le document
+// Bouton vers le PDF
+document.querySelector(".bouton").innerHTML = `<a href='pdf/${page}.pdf' target='_blank'>
+    <div>Ouvrir le document</div>
+    <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='none' stroke='#F5F5DC' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><path d='M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8'/></svg>
+</a>`;
 
-document.querySelector(".bouton").innerHTML = "<a href='../pdf/" + page + ".pdf' target='_blank'><div>Ouvrir le document</div><svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 24 24' fill='none' stroke='#F5F5DC' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><g fill='none' fill-rule='evenodd'><path d='M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8'/></g></svg></a>";
+// Création de l'accordéon
+let ac_num = Object.keys(SAE[page]["AC"]);
+let accordeon_html = "";
 
-let apprentissages = Object.keys(SAE[page]["AC"]);
-
-let descriptionsAC = Object.keys(ACRITIQUES[page]);
-
-let accordeon_ac = "";
-
-apprentissages.forEach(function (ac, i) {
-  accordeon_ac += "<button class='accordeon'>" + apprentissages[i] + " - " + SAE[page]["AC"][ac] + "</button><div class='contenu'><p>Description de l'AC : " + ACRITIQUES[page][ac] + "</p></div>";
+ac_num.forEach(function (ac_num) {
+  accordeon_html += `
+        <button class='accordeon'>${ac_num} - ${SAE[page]["AC"][ac_num]}</button>
+        <div class='contenu'>
+            <p><strong>Détail :</strong> ${ACRITIQUES[page][ac_num]}</p>
+        </div>`;
 })
 
-document.querySelector(".ac").innerHTML = accordeon_ac;
+document.querySelector(".ac").innerHTML = accordeon_html;
 
-var ac = document.getElementsByClassName("accordeon");
-var i;
+// Interactivité de l'accordéon
+const ac_elements = document.querySelectorAll(".accordeon");
 
-for (i = 0; i < ac.length; i++) {
-  ac[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+ac_elements.forEach(function(bouton) {
+    bouton.addEventListener("click", function () {
+        // Bascule la classe "active" pour l'animation de la flèche
+        this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
-    var contenu = this.nextElementSibling;
-    if (contenu.style.display === "block") {
-      contenu.style.display = "none";
-    } else {
-      contenu.style.display = "block";
-    }
-  });
-}
-
-//
+        // Gestion de l'affichage du panneau (nextElementSibling cible la div .contenu)
+        const panneau = this.nextElementSibling;
+        if (panneau.style.display === "block") {
+            panneau.style.display = "none";
+        } else {
+            panneau.style.display = "block";
+        }
+    });
+});
